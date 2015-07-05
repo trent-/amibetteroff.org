8b2581143757977fab6b6f7c5f77dc4f4a371dd9
Trent Schafer <trent.schafer@gmail.com>
2015-07-05 11:11:44 +1000

    Add source files

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# money-what-money
+# Amibetteroff.org
+
+Am I better off is a project that was created a part of the GovHack 2015 event.
+
+The idea of this project is to give people the opportunity to see what the cost of living differences will be in different geographical area's.
+
+The web interface was built with Oracle Application Express, and uses a theme developed by [Vincent Morneau](https://github.com/vincentmorneau/material-apex), which is inspired by Google Material Design.
+
+## Data
+
+To make this project possible, we utilised data from both the Australian Taxation Office (ATO) and the Australian Bureau of Statistics (ABS) - both for the 2013 financial year.
+
+1. ATO - Tax table 5: https://data.gov.au/dataset/taxation-statistics-2012-13/resource/5be9bac4-8bbd-45bb-a686-b7f017260699
+2. ABS - Residential Dwellings: Values, Mean Prices and Number by State and Territories: http://govhack.abs.gov.au/Index.aspx?themetreeid=10
+
+## Installation
+
+1. Create a schema for your database
+*. Run the script scripts/ddl.sql
+* Run the script scripts/data.sql
+* Create a workspace in Application Express
+* Import the application export file f102381.sql
+
+## Usage
---
+++ scripts/ddl.sql
@@ -0,0 +1,200 @@
+create table tax_year_people(
+    ID NUMBER PRIMARY KEY,
+    YEAR_END NUMBER(4) NOT NULL,
+    NUM_PEOPLE NUMBER NOT NULL
+);
+/
+
+create sequence tax_year_people_seq;
+/
+
+create or replace trigger bi_tax_year_people
+before insert on tax_year_people
+for each row
+begin
+    :NEW.ID := tax_year_people_seq.nextval;
+end bi_tax_year_people;
+/
+
+
+create table tax_state_people(
+    ID NUMBER PRIMARY KEY,
+    YEAR_END NUMBER(4) NOT NULL,
+    AUS_STATE VARCHAR2(3),
+    TAXED_PEOPLE NUMBER NOT NULL
+);
+/
+
+create sequence tax_state_people_seq;
+/
+
+create or replace trigger bi_tax_state_people
+before insert on tax_state_people
+for each row
+begin
+    :NEW.ID := tax_state_people_seq.nextval;
+end bi_tax_state_people;
+/
+
+
+alter table tax_State_people
+add constraint "TAX_STATE_PEOPLE_UK1" UNIQUE ("YEAR_END", "AUS_STATE");
+/
+
+
+create table tax_brackets(
+    ID NUMBER PRIMARY KEY,
+    start_amount NUMBER NOT NULL,
+    end_amount NUMBER
+);
+/
+
+create sequence tax_brackets_seq;
+/
+
+create or replace trigger bi_tax_brackets
+before insert on tax_brackets
+for each row
+begin
+    :NEW.ID := tax_brackets_seq.nextval;
+end bi_tax_brackets;
+/
+
+
+alter table tax_brackets
+add constraint "TAX_BRACKETS_UK1" UNIQUE (start_amount, end_amount);
+/
+
+
+
+
+create table industry_codes(
+    ID NUMBER PRIMARY KEY,
+    INDUSTRY_CODE VARCHAR2(1) NOT NULL,
+    INDUSTRY_NAME VARCHAR2(50) NOT NULL
+);
+/
+
+create sequence industry_codes_seq;
+/
+
+create or replace trigger bi_industry_codes
+before insert on industry_codes
+for each row
+begin
+    :NEW.id := industry_codes_seq.nextval;
+end bi_industry_codes_seq;
+/
+
+
+alter table industry_codes
+add constraint "INDUSTRY_CODES_UK1" UNIQUE ("INDUSTRY_CODE");
+/
+
+create table area (
+    ID NUMBER PRIMARY KEY,
+    AREA_CODE VARCHAR2(3) NOT NULL,
+    AREA_NAME VARCHAR2(15) NOT NULL
+);
+/
+
+create sequence area_seq;
+/
+
+create or replace trigger bi_area
+before insert on area
+for each row
+begin
+    :NEW.ID := area_seq.nextval;
+end bi_area;
+/
+
+alter table area
+add constraint "AREA_UK1" UNIQUE ("AREA_CODE");
+/
+
+
+create table taxation_income (
+    ID NUMBER PRIMARY KEY,
+    AREA_CODE VARCHAR2(3),
+    GENDER varchar2(6) NOT NULL,
+    INDUSTRY_CODE varchar2(1) NOT NULL,
+    NUM_PEOPLE NUMBER NOT NULL,
+    TOTAL_TAX_DOLLARS NUMBER NOT NULL,
+    GROSS_TAX_DOLLARS NUMBER NOT NULL,
+    NET_TAX_DOLLARS NUMBER NOT NULL
+);
+/
+
+create sequence taxation_income_seq;
+/
+
+create or replace trigger bi_taxation_income
+before insert on taxation_income
+for each row
+begin
+    :NEW.ID := taxation_income_seq.nextval;
+end bi_taxation_income;
+/
+
+alter table taxation_income
+add constraint "TAXATION_INCOME_FK1" FOREIGN KEY ("AREA_CODE") REFERENCES "AREA" ("AREA_CODE");
+/
+
+alter table taxation_income
+add constraint "INDUSTRY_CODE_FK2" FOREIGN KEY ("INDUSTRY_CODE") REFERENCES "INDUSTRY_CODES" ("INDUSTRY_CODE");
+/
+
+CREATE OR REPLACE TRIGGER "BI_TAXATION_INCOME"
+before insert on taxation_income
+for each row
+begin
+  :NEW.ID := taxation_income_seq.nextval;
+end bi_taxation_income;
+/
+ALTER TRIGGER "BI_TAXATION_INCOME" ENABLE;
+/
+
+--
+
+create table state_expenditure(
+    ID NUMBER PRIMARY KEY,
+    STATE VARCHAR2(3) NOT NULL,
+    CATEGORY varchar2(26) NOT NULL,
+    WEEKLY_SPEND NUMBER
+);
+/
+
+
+create sequence state_expenditure_seq;
+    /
+
+
+    CREATE OR REPLACE TRIGGER "BI_STATE_EXPENDITURE"
+    before insert on STATE_EXPENDITURE
+    for each row
+    begin
+      :NEW.ID := state_expenditure_seq.nextval;
+    end BI_STATE_EXPENDITURE;
+    /
+
+
+create table mean_house_prices(
+    ID NUMBER PRIMARY KEY,
+    "QUARTER" VARCHAR2(26 BYTE),
+	"STATE" VARCHAR2(3 BYTE),
+	"YEAR" NUMBER,
+	"MEAN_PRICE" NUMBER
+);
+/
+
+create sequence mean_house_prices_seq;
+    /
+
+    CREATE OR REPLACE TRIGGER "BI_MEAN_HOUSE_PRICES"
+    before insert on MEAN_HOUSE_PRICES
+    for each row
+    begin
+      :NEW.ID := mean_house_prices_seq.nextval;
+    end BI_MEAN_HOUSE_PRICES;
+    /
